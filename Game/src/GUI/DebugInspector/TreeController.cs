6edc787cf75ef0cf7ff40420997415809e5d03d6
Sunshine-dev-forever <ajmckinney370@gmail.com>
2023-09-09 13:58:59 -0700

    fix some scenes that could not be opened

--- Game/src/GUI/DebugInspector/TreeController.cs
+++ Game/src/GUI/DebugInspector/TreeController.cs
@@ -14,21 +14,8 @@ namespace UI.DebugInspector
 		public event ItemSelected? ItemSelected;
 		public Tree Tree { get; private set; } = null!;
 		// Called when the node enters the scene tree for the first time.
-		public override void _Ready()//asdf
+		public override void _Ready()
 		{
-			Tree = GetNode<Tree>("Tree");
-			TreeItem root = Tree.CreateItem();
-			root.SetText(0, "ROOT ROOT");
-
-			TreeItem child1 = root.CreateChild();
-			child1.SetMetadata(0, "I am the good child!");
-			child1.SetText(0, "Angels and shit!");
-
-			TreeItem child2 = root.CreateChild();
-			child2.SetMetadata(0, "I am the bad child");
-			child2.SetText(0, "Demons and Such");
-			//Column should always be 0 as far as I can tell
-
 			Tree.CellSelected += HandleCellSelected;
 
 		}
